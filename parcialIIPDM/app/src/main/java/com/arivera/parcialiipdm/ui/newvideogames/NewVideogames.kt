17c48a942a0452f9eb339d0e8b34aec7f86bf79c
Alexandra Rivera <00162121@uca.edu.sv>
2023-05-23 10:05:43 -0600

    add: Capa Viewmodel VideogamesViewmodel

--- parcialIIPDM/app/src/main/java/com/arivera/parcialiipdm/ui/newvideogames/NewVideogames.kt
+++ parcialIIPDM/app/src/main/java/com/arivera/parcialiipdm/ui/newvideogames/NewVideogames.kt
@@ -1,18 +1,19 @@
-package com.arivera.parcialiipdm
+package com.arivera.parcialiipdm.ui.newvideogames
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.arivera.parcialiipdm.R
 
-class VideogamesList : Fragment() {
+class NewVideogames : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_videogames_list, container, false)
+        return inflater.inflate(R.layout.fragment_new_videogames, container, false)
     }
 }
